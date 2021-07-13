685ac5131722b94fd8a8cab99f8d973c146746f4
Johan Strauss <576346@student.belgiumcampus.ac.za>
2021-07-13 17:24:31 +0200

    Added some fixes to extract.js

--- extract.js
+++ extract.js
@@ -6,48 +6,69 @@ const fs = require('fs');
 var path = require('path');
 
 async function extractZip(source, target) {
-	try {
-	  await extract(source, { dir: target });
-	  console.log("Extraction complete");
-	} catch (err) {
-	  console.log("Oops: extractZip failed", err);
-	}
-}	  
+    try {
+      await extract(source, { dir: target });
+      console.log("Extraction complete");
+    } catch (err) {
+      console.log("Oops: extractZip failed", err);
+    }
+}
 
 const unzipFiles = async function (dirPath) {
 
-	if (dirPath.endsWith('.zip')){
-		await unzipRoot(dirPath);
-		const newDirPath = dirPath.replace(".zip", "");
-		await unzipFiles(newDirPath);
-	}
-	else{
-		const files = fs.readdirSync(dirPath);
-  
-		await Promise.all(
-		  files.map(async (file) => {
-			if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-			  await unzipFiles(dirPath + "/" + file);
-			} else {
-			  const fullFilePath = path.join(dirPath, "/", file);
-			  const folderName = file.replace(".zip", "");
-			  if (file.endsWith(".zip")) {
-				await extractZip(fullFilePath, path.join(dirPath, "/", folderName));
-				await unzipFiles(dirPath + "/" + folderName);
-			  }
+   
+        if (dirPath.endsWith('.zip')){
+            await unzipRoot(dirPath);
+            const newDirPath = dirPath.replace(".zip", "");
+            await unzipFiles(newDirPath);
+        }
+        else{
+			if (fs.existsSync(path)){
+            const files = fs.readdirSync(dirPath);
+
+            await Promise.all(
+              files.map(async (file) => {
+                if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+                  await unzipFiles(dirPath + "/" + file);
+                } else {
+                  const fullFilePath = path.join(dirPath, "/", file);
+                  const folderName = file.replace(".zip", "");
+                  if (file.endsWith(".zip")) {
+                    await extractZip(fullFilePath, dirPath);
+                    await unzipFiles(dirPath + "/" + folderName);
+                  }
+                }
+              })
+            );
 			}
-		  })
-		);
-	}	
+        }
+    
 };
 
 const unzipRoot = async function (dirPath) {
-	const newRoot = dirPath.replace(".zip", "");
+	
+    const newRoot = dirPath.replace(".zip", "");
 
-	await extractZip(dirPath, newRoot);
+    await extractZip(dirPath, newRoot);
 };
+
+function removeZips(read) {
+    const files = fs.readdirSync(read);
+    for (const file in files) {
+        try {
+            if (files[file].endsWith('.zip')) {
+                remove.removeSync(read + "/" + files[file]);
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }
+}
 
 //unzipFiles(dirPath1);
 //removeDir(dirPath1);
 
-module.exports = unzipFiles;
+module.exports = {unzipFiles,removeZips};
+
+
+//C:/Users/Johan Strauss/Documents/Uni/3rd Year/WPR381/TestData.zip
